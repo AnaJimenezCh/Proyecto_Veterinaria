144e467806c792899e1019d2455e3e77ff5763b8
AnaJimenezCh <ajimenez20058@ufide.ac.cr>
2023-07-26 14:08:45 -0600

    Cambios en la interfaz

--- src/main/java/com/ProyectoVeterinaria/Controller/IndexController.java
+++ src/main/java/com/ProyectoVeterinaria/Controller/IndexController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
- */
+
 package com.ProyectoVeterinaria.Controller;
 
 import org.springframework.stereotype.Controller;
@@ -17,7 +14,7 @@ public class IndexController {
     
     @RequestMapping("/")
     public String page(Model model) {
-        model.addAttribute("mensaje", "hola desde el controllador");
+        //model.addAttribute("mensaje", "hola desde el controllador");
         return "index";
     }
     
