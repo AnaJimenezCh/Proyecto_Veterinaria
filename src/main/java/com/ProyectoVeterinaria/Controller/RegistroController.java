e72c89bc8332e4c7f9bbe25f9d904690a8e3931e
AnaJimenezCh <ajimenez20058@ufide.ac.cr>
2023-07-26 14:08:13 -0600

    Cambios en la interfaz

---
+++ src/main/java/com/ProyectoVeterinaria/Controller/RegistroController.java
@@ -0,0 +1,31 @@
+
+package com.ProyectoVeterinaria.Controller;
+
+import com.ProyectoVeterinaria.domain.Registro;
+import com.ProyectoVeterinaria.service.RegistroService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author AnaJimenezCh
+ */
+@Controller
+public class RegistroController {
+    
+   @RequestMapping("/acerca")
+    public String page(Model model) {
+        //model.addAttribute("mensaje", "hola desde el controllador");
+        return "acerca";
+    
+   
+        
+        
+    }
+    
+}
