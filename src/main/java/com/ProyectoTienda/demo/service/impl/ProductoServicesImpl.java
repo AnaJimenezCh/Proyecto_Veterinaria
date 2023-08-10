0d80188217596e1bf56537628ded83a9016f8d10
Eduardo <Eduardo@DESKTOP-6OCH79S>
2023-08-09 21:37:14 -0600

    Semana 10

---
+++ src/main/java/com/ProyectoTienda/demo/service/impl/ProductoServicesImpl.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.ProyectoTienda.demo.service.impl;
+
+import com.ProyectoTienda.demo.dao.ProductoDao;
+import com.ProyectoTienda.demo.domain.Producto;
+import com.ProyectoTienda.demo.service.ProductoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductoServicesImpl implements ProductoService {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activos) {
+        List<Producto> lista = productoDao.findAll();
+
+        if (activos) {
+            // para remover las Productos donde estan activos
+            lista.removeIf(x -> !x.isActivo());
+        }
+
+        return lista;
+
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto getProducto(Producto producto) {
+        return productoDao.findById(producto.getIdProducto()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Producto producto) {
+        productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+    }
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
+    }
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }    
+}
